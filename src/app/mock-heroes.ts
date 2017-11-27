e06a5bc98269500b78ef0ec7b76b27fac0812937
Muhammad Jawwad <muhammad.jawwad@systemsltd.local>
2017-11-27 13:00:39 +0500

    Prepare ProjectX (First Commit)

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Wonderful' },
+    { id: 12, name: 'Yokuzuna' },
+    { id: 13, name: 'Magneto' },
+    { id: 14, name: 'Naruto' },
+    { id: 15, name: 'Wolverine' },
+    { id: 16, name: 'Spiderman' },
+    { id: 17, name: 'Ironman' },
+    { id: 18, name: 'Batman' },
+    { id: 19, name: 'Hulk' },
+    { id: 20, name: 'Rock' },
+];
